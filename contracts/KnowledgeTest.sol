40c7553f0496e92e4c5e9551e6c1d86f53e2d1ff
Reid Simonton <simonton@gmail.com>
2022-08-05 10:52:03 -0700

    done

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -6,8 +6,42 @@ contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
+    address public owner;
+
+    modifier soloElGuapo() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {
+
+    }
+
+    function changeTokens() public {
         string[] memory t = tokens;
         t[0] = "VET";
+        tokens[0] = t[0];
     }
+
+    function concatenate(string calldata s1, string calldata s2) public pure returns (string memory) {
+        return string.concat(s1, s2);
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function transferAll(address to) public soloElGuapo returns (bool) {
+        (bool success, ) = to.call{value: getBalance()}("");
+        return success;
+    }
+
 }
